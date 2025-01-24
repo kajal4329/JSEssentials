b7297ab78ce455ebaf375c56a62956a1ae913c38
Kajal4329 <kajalsingh4329@gmail.com>
2025-01-24 09:24:08 -0500

    Added delete button

--- ClassesAndObjects/BookManagementSystem/book_system.js
+++ ClassesAndObjects/BookManagementSystem/book_system.js
@@ -23,7 +23,9 @@ function showbooks() {
         <p><strong>Book Name: </strong>${book.name}</p>
         <p><strong>Author Name:</strong> ${book.authorName}</p>
         <p><strong>Book Description:</strong> ${book.bookDescription}</p>
-        <p><strong>No. of Pages:</strong> ${book.pagesNumber} page(s)</p>`
+        <p><strong>No. of Pages:</strong> ${book.pagesNumber} page(s)</p>
+        <button onclick="deleteBook(${index})">Delete</button>`
+        
     );
     document.getElementById('books').innerHTML = booksDiv.join('');
 }
@@ -34,3 +36,8 @@ function clearInputs() {
             document.getElementById('bookDescription').value = '';
             document.getElementById('pagesNumber').value = '';
  }
+
+function deleteBook(index){
+    books.splice(index,1);
+    showbooks();
+}
